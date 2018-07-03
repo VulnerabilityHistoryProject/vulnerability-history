d8a25f788827100dbbbdd792f96c2d8aef475c7e
Andy Meneely <andy@se.rit.edu>
2018-07-03 18:55:36 -0400

    add curate page

---
+++ app/helpers/curate_helper.rb
@@ -0,0 +1,2 @@
+module CurateHelper
+end
---
+++ test/controllers/curate_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CurateControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
