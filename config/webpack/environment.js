038c662ea20ad6084766730cd7e10a09f0a792e1
Andy Meneely <andy@se.rit.edu>
2018-09-25 16:25:16 -0400

    add coffee support, add app/assets

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const coffee =  require('./loaders/coffee')
 const erb =  require('./loaders/erb')
 
 environment.loaders.append('erb', erb)
+environment.loaders.append('coffee', coffee)
 module.exports = environment
